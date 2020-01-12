f54eb8b5085e76eebdd0ebd2fb93497dfb9b2dee
Kdavid2355 <einjune@naver.com>
2020-01-12 19:38:21 +0900

    2020/1/12

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 from flask import session, redirect, url_for
+from flask_session import Session
 
 
 
@@ -9,12 +10,20 @@ db = client.dbexhibition
 
 
 app = Flask(__name__)
+sess = Session()
+SESSION_TYPE=['redis']
+
+app.config.from_object(__name__)
+Session(app)
+
 
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    userid = session.get('userid',None)
+    return render_template('home.html', userid=userid)
+
 
 
 
@@ -23,7 +32,71 @@ def home():
 def listing():
     result = list(db.exhibitions.find({},{'_id':0}))
     return jsonify({'result':'success' , 'exhibitions':result})
+
+@app.route('/logout')
+def logout():
+    session.pop('username',None)
+    return redirect('/')
+
+
+@app. route('/login', methods=['POST','GET'])
+def login():
+
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    userid_receive = request.form['userid']
+    password_receive = request.form['password']
+
+    result_id = db.users.find_one({'password':password_receive})
     
+    try : 
+        if userid_receive == result_id['userid '] and password_receive == result_id['password']:
+            session['userid'] = request.form['userid']
+            return redirect('/') 
+
+    except TypeError:
+        print('다시입력하세요!')
+        return render_template('login.html')      
+
+
+
+    #print(db.users.find({},{'_id':0},{'userid':userid_receive, 'password':password_receive}))
+    # print(result_id['userid '])
+    # if userid_receive == result_id['userid '] and password_receive == result_id('password'): 
+    #     return render_template('home.html')
+    #     #return render_template('home.html') 
+
+    return render_template('login.html')
+
+@app.route('/register',  methods=['POST','GET'])
+def register():
+
+    if request.method == 'GET':
+        return render_template('register.html')
+    
+    userid = request.form['userid']
+    password = request.form['password']
+    repassword = request.form['re-password']
+
+    if password == repassword :
+
+        doc = {
+            'userid ' : userid,
+            'password' : password
+        }
+
+        db.users.insert_one(doc)
+
+        db.session.add(users)
+        db.session.commit()
+        print('SUCCESS')
+
+        return redirect('/')
+
+    return render_template('register.html')
+    
+
 
 
 @app.route('/review')
@@ -32,7 +105,10 @@ def review():
 
 
 
-
 if __name__ == '__main__':
 
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.secret_key = 'qwertyu'
+    
+    sess.init_app(app)
+    
+    app.run('127.0.0.1', port=5000, debug=True)
--- scrapping.py
+++ scrapping.py
@@ -10,20 +10,19 @@ db = client.dbexhibition
 # 타겟 URL을 읽어서 HTML를 받아오고,
 headers = {'Host' : 'search.naver.com'}
 rank = 0
-url1 = 'https://search.naver.com/p/csearch/content/qapirender.nhn?key=PerformListAPI&where=nexearch&pkid=269&_callback=jQuery1124007947377311697013_1577775357992&q=++%EA%B3%B5%EC%97%B0&so=&start='
-for i in range(1,5):
-    data = requests.get(url1 + str(i))
-#data = requests.get('https://search.naver.com/p/csearch/content/qapirender.nhn?key=PerformListAPI&where=nexearch&pkid=269&_callback=jQuery1124007947377311697013_1577775357992&q=++%EA%B3%B5%EC%97%B0&so=&start=1', headers=headers)
+#url1 = 'https://search.naver.com/p/csearch/content/qapirender.nhn?key=PerformListAPI&where=nexearch&pkid=269&_callback=jQuery1124007947377311697013_1577775357992&q=++%EA%B3%B5%EC%97%B0&so=&start='
+
+data = requests.get('https://search.naver.com/p/csearch/content/qapirender.nhn?key=PerformListAPI&where=nexearch&pkid=269&_callback=jQuery1124007947377311697013_1577775357992&q=++%EA%B3%B5%EC%97%B0&so=&start=1', headers=headers)
 # HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
 # soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
 # 이제 코딩을 통해 필요한 부분을 추출하면 된다.
 
-    regex = re.compile(r'\((.*)\)')
-    match = regex.search(data.text)
-    gr = match.group(1)
-    json_data = json.loads(gr)
-    html_data = json_data['list'][0]['html']
-    soup = BeautifulSoup(html_data, 'html.parser')
+regex = re.compile(r'\((.*)\)')
+match = regex.search(data.text)
+gr = match.group(1)
+json_data = json.loads(gr)
+html_data = json_data['list'][0]['html']
+soup = BeautifulSoup(html_data, 'html.parser')
 # img src 뽑아오기
 #형식
 # < div
@@ -82,25 +81,26 @@ for i in range(1,5):
 # (입맛에 맞게 코딩)
 #############################
 
-    exhibition = soup.select('.list_title ')
+exhibition = soup.select('.list_title ')
 
 
-    for ex in exhibition:
-        a_tag = ex.find_all('a')
-        if a_tag is not None:
-            name = a_tag[0].text
-            img = soup.find("img")
-            img_src = img.get("src")
-            rank += 1
-            print(rank, name, img_src)
+for ex in exhibition:
+    a_tag = ex.find_all('a')
+    if a_tag is not None:
+        #name = a_tag[0].text
+        img = soup.find("img")
+        name = img.get("alt")
+        img_src = img.get("src")
+        rank += 1
+        print(rank, name, img_src)
 
-            doc = {
-                'rank' : rank,
-                'name' : name,
-                'img' : img_src
-            }
+        doc = {
+            'rank' : rank,
+            'name' : name,
+            'img' : img_src
+        }
 
-            db.exhibitions.insert_one(doc)
+        db.exhibitions.insert_one(doc)
 
 
 
